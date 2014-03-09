aed34aff3308a4c4fe9cfe2e57a2bc6c9b591334
Sumit Tripathi <sumit-8-5-tripathi@gmail.com>
2014-03-10 00:12:03 +0530

    Test 2 commit.

---
+++ Test/Test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by Sumit Kumar on 10/03/14.
+//  Copyright (c) 2014 Sumit Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Test/Test/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by Sumit Kumar on 10/03/14.
+//  Copyright (c) 2014 Sumit Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
